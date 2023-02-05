6aa7c81efebc991f87685346062d94311c6f8563
sitirgon <krzysiu.gratka@o2.pl>
2023-02-05 13:05:03 +0100

    File exists exception, Done read_xml function, fix add_to_xml, add xml_Declaration and encoding UTF-8

--- main.py
+++ main.py
@@ -9,32 +9,28 @@ class Xml_adapter:
         self.master = 'Config'
         self.config_name = config
         check_file = os.path.isfile(self.config_name)
-        #TODO: expect file is exist
         if check_file:
             return
         elif not check_file:
             data_xml = ET.Element(self.master)
-            b_xml = ET.tostring(data_xml)
-            with open(self.config_name, 'wb') as f:
-                f.write(b_xml)
+            b_xml = ET.ElementTree(data_xml)
+            b_xml.write(open(self.config_name, 'wb'), encoding='UTF-8', xml_declaration=True)
 
-    def read_xml(self):
-        root = ET.parse(self.config_name).getroot()
+    def read_xml(self, subElement_tag: str = None):
+        if subElement_tag is not None:
+            root = ET.parse(self.config_name).getroot()
+            return root.find(subElement_tag)
 
-    def add_to_xml(self, sub_tag: str = None, sub_text: str = None):
+    def add_to_xml(self, subElement_tag: str = None, subElement_text: str = None):
         root = ET.parse(self.config_name).getroot()
         newElement = ET.Element(self.master)
-        newSubElement = ET.SubElement(newElement, sub_tag)
-        newSubElement.text = sub_text
+        newSubElement = ET.SubElement(newElement, subElement_tag)
+        newSubElement.text = subElement_text
         root.insert(0, newSubElement)
         tree = ET.ElementTree(root)
-        tree.write(open(self.config_name, 'w'), encoding='unicode')
+        tree.write(open(self.config_name, 'wb'), encoding='UTF-8', xml_declaration=True)
 
 
 if __name__ == '__main__':
-    a = Xml_adapter()
-    b = input()
-    a.add_to_xml(sub_text='https://google.pl/', sub_tag='Google')
-    c = input()
-    a.add_to_xml(sub_tag='Zooplus', sub_text='https://zooplus.pl/')
-    c = input()
+    pass
+
