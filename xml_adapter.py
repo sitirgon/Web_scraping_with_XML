0b2655a7946da2360f3739bf8aad229b28f4f319
sitirgon <krzysiu.gratka@o2.pl>
2023-02-06 18:13:41 +0100

    Change logic remove_from_xml, correct return read_xml

--- xml_adapter.py
+++ xml_adapter.py
@@ -13,12 +13,17 @@ class XmlAdapter:
         elif not check_file:
             data_xml = et.Element(self.master)
             b_xml = et.ElementTree(data_xml)
-            b_xml.write(open(self.config_name, 'wb'), encoding='UTF-8', xml_declaration=True)
+            b_xml.write(self.config_name, encoding='UTF-8', xml_declaration=True)
 
     def read_xml(self, sub_element_tag: str = None):
+        root = et.parse(self.config_name).getroot()
         if sub_element_tag is not None:
-            root = et.parse(self.config_name).getroot()
-            return root.find(sub_element_tag)
+            return root.findall(sub_element_tag)
+        elif sub_element_tag is None:
+            tree = et.ElementTree(root)
+            return [(i.tag, i.text)
+                    for i in tree.iter()
+                    if i.text is not None]
 
     def add_to_xml(self, sub_element_tag: str = None, sub_element_text: str = None):
         root = et.parse(self.config_name).getroot()
@@ -27,4 +32,15 @@ class XmlAdapter:
         new_sub_element.text = sub_element_text
         root.insert(0, new_sub_element)
         tree = et.ElementTree(root)
-        tree.write(open(self.config_name, 'wb'), encoding='UTF-8', xml_declaration=True)
+        tree.write(self.config_name, encoding='UTF-8', xml_declaration=True)
+
+    def remove_from_xml(self, sub_element_tag: str = None):
+        root = et.parse(self.config_name).getroot()
+        print(root.findall(sub_element_tag))
+        if root.findall(sub_element_tag):
+            root.remove(root.find(sub_element_tag))
+            tree = et.ElementTree(root)
+            tree.write(self.config_name, encoding='UTF-8', xml_declaration=True)
+        elif not root.findall(sub_element_tag):
+            return 0
+
